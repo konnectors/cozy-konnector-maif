080eafbd74ca5f1a8e12381520557ee91c606706
Grégory Le Garec <gregory.legarec@gmail.com>
2017-09-01 15:49:49 +0200

    feat: log cozyfield for production debug :speaker:

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cozyFields = JSON.parse(process.env.COZY_FIELDS)
 log('info', 'cozyfields', cozyFields)
 
 konnector.fetch({account: cozyFields.account, folderPath: cozyFields.folder_to_save}, err => {
+  log('error', 'cozyfields', cozyFields)
   log('debug', 'The konnector has been run')
   if (err) {
     log('error', err)
