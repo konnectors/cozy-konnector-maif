53a29978e74869e881b4c101832c232e0d7d0341
Grégory Le Garec <gregory.legarec@gmail.com>
2017-09-01 18:46:52 +0200

    feat: updating error log level to debug :pencil:

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {log} = require('cozy-konnector-libs')
 const cozyFields = JSON.parse(process.env.COZY_FIELDS)
 
 konnector.fetch({account: cozyFields.account, folderPath: cozyFields.folder_to_save}, err => {
-  log('error', cozyFields.folder_to_save)
+  log('debug', cozyFields.folder_to_save)
   log('debug', 'The konnector has been run')
   if (err) {
     log('error', err)
