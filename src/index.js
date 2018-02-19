a0486c1346b9e2fa09b50f598c2bc0f5a870dfe3
Simon Constans <simonc@linux.com>
2018-02-19 19:04:46 +0100

    chore: Add format command 💉

--- src/index.js
+++ src/index.js
@@ -1,58 +1,89 @@
-const moment = require('moment')
-const {BaseKonnector, cozyClient, log, requestFactory, updateOrCreate} = require('cozy-konnector-libs')
+const moment = require("moment");
+const {
+  BaseKonnector,
+  cozyClient,
+  log,
+  requestFactory,
+  updateOrCreate
+} = require("cozy-konnector-libs");
 
-const apikey = 'eeafd0bd-a921-420e-91ce-3b52ee5807e8'
-const infoUrl = `https://openapiweb.maif.fr/prod/cozy/v1/mes_infos?apikey=${apikey}`
+const apikey = "eeafd0bd-a921-420e-91ce-3b52ee5807e8";
+const infoUrl = `https://openapiweb.maif.fr/prod/cozy/v1/mes_infos?apikey=${apikey}`;
 
-const REQUEST_TIMEOUT_MS = 10000
+const REQUEST_TIMEOUT_MS = 10000;
 
 let request = requestFactory({
   // debug: true,
-})
+});
 
-module.exports = new BaseKonnector(function fetch (fields) {
-  const fetch = isTokenExpired(fields.access_token) ? renewAndFetchData : fetchData
+module.exports = new BaseKonnector(function fetch(fields) {
+  const fetch = isTokenExpired(fields.access_token)
+    ? renewAndFetchData
+    : fetchData;
 
-  return fetch.bind(this)(fields)
+  return fetch
+    .bind(this)(fields)
     .then(response => normalizeResponse.bind(this)(response))
     .then(entries => {
-      return updateOrCreate(entries.contrats, 'fr.maif.maifuser.contrat', ['societaire'])
-        .then(() => updateOrCreate(entries.homes, 'fr.maif.maifuser.home', ['name']))
-        .then(() => updateOrCreate(entries.foyers, 'fr.maif.maifuser.foyer', ['name']))
-        .then(() => updateOrCreate(entries.paymenttermss, 'fr.maif.maifuser.paymentterms', ['societaire']))
-        .then(() => updateOrCreate(entries.sinistres, 'fr.maif.maifuser.sinistre', ['timestamp']))
-        .then(() => updateOrCreate(entries.societaires, 'fr.maif.maifuser.societaire', ['email']))
-    }).catch(err => {
-      log('error', JSON.stringify(err))
-      this.terminate('VENDOR_DOWN')
+      return updateOrCreate(entries.contrats, "fr.maif.maifuser.contrat", [
+        "societaire"
+      ])
+        .then(() =>
+          updateOrCreate(entries.homes, "fr.maif.maifuser.home", ["name"])
+        )
+        .then(() =>
+          updateOrCreate(entries.foyers, "fr.maif.maifuser.foyer", ["name"])
+        )
+        .then(() =>
+          updateOrCreate(
+            entries.paymenttermss,
+            "fr.maif.maifuser.paymentterms",
+            ["societaire"]
+          )
+        )
+        .then(() =>
+          updateOrCreate(entries.sinistres, "fr.maif.maifuser.sinistre", [
+            "timestamp"
+          ])
+        )
+        .then(() =>
+          updateOrCreate(entries.societaires, "fr.maif.maifuser.societaire", [
+            "email"
+          ])
+        );
     })
-})
+    .catch(err => {
+      log("error", JSON.stringify(err));
+      this.terminate("VENDOR_DOWN");
+    });
+});
 
 // for tests
-module.exports.renewToken = renewToken
+module.exports.renewToken = renewToken;
 
-function renewToken (requiredFields) {
-  const accountId = JSON.parse(process.env.COZY_FIELDS).account
-  return cozyClient.fetchJSON('POST', `/accounts/maif/${accountId}/refresh`)
+function renewToken(requiredFields) {
+  const accountId = JSON.parse(process.env.COZY_FIELDS).account;
+  return cozyClient
+    .fetchJSON("POST", `/accounts/maif/${accountId}/refresh`)
     .then(body => {
-      requiredFields.access_token = body.attributes.oauth.access_token
-    // log('info', requiredFields.access_token, 'new access_token')
-    })
+      requiredFields.access_token = body.attributes.oauth.access_token;
+      // log('info', requiredFields.access_token, 'new access_token')
+    });
 }
 
-function isTokenExpired (token) {
-  var base64Url = token.split('.')[1]
-  var base64 = base64Url.replace('-', '+').replace('_', '/')
-  var decodedToken = JSON.parse(Buffer.from(base64, 'base64'))
-  var decodedTimestamp = decodedToken.exp * 1000
-  var timestamp = Date.now()
+function isTokenExpired(token) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace("-", "+").replace("_", "/");
+  var decodedToken = JSON.parse(Buffer.from(base64, "base64"));
+  var decodedTimestamp = decodedToken.exp * 1000;
+  var timestamp = Date.now();
 
   if (decodedTimestamp > timestamp) {
-    log('info', 'Token non expiré ... Fetching des Data')
-    return false
+    log("info", "Token non expiré ... Fetching des Data");
+    return false;
   } else {
-    log('info', 'Token expiré ... Renouvellement du token')
-    return true
+    log("info", "Token expiré ... Renouvellement du token");
+    return true;
   }
 }
 
@@ -60,19 +91,19 @@ function isTokenExpired (token) {
  * The API sends the Euro sign as the 128 character (80 in hex)
  * We convert it here
  */
-function convert128ToEuro (s) {
-  if (s && typeof s.replace === 'function') s = s.replace(/\u0080/, '€')
-  return s
+function convert128ToEuro(s) {
+  if (s && typeof s.replace === "function") s = s.replace(/\u0080/, "€");
+  return s;
 }
 
-function cleanHomeData (homeData) {
+function cleanHomeData(homeData) {
   return Object.assign({}, homeData, {
     patrimoineMobilier: convert128ToEuro(homeData.patrimoineMobilier)
-  })
+  });
 }
 
-function fetchData (fields) {
-  log('info', 'fetchData')
+function fetchData(fields) {
+  log("info", "fetchData");
 
   return request({
     url: infoUrl,
@@ -81,64 +112,66 @@ function fetchData (fields) {
     },
     timeout: REQUEST_TIMEOUT_MS,
     resolveWithFullResponse: true
-  })
+  });
 }
 
-function renewAndFetchData (fields) {
-  return renewToken(fields)
-    .then(() => fetchData(fields))
+function renewAndFetchData(fields) {
+  return renewToken(fields).then(() => fetchData(fields));
 }
 
-function normalizeResponse (response) {
-  moment.locale('fr')
-  const body = response.body
-  const entries = {}
+function normalizeResponse(response) {
+  moment.locale("fr");
+  const body = response.body;
+  const entries = {};
 
-  if (body && body['MesInfos']) {
+  if (body && body["MesInfos"]) {
     // Ajout data Contrat
-    entries.contrats = []
-    entries.contrats.push({'contrat': body['MesInfos'].contract})
+    entries.contrats = [];
+    entries.contrats.push({ contrat: body["MesInfos"].contract });
 
     // Ajout data Home
-    entries.homes = []
-    if (body['MesInfos'].home && typeof body['MesInfos'].home.map === 'function') {
-      entries.homes.push({'home': body['MesInfos'].home.map(cleanHomeData)})
+    entries.homes = [];
+    if (
+      body["MesInfos"].home &&
+      typeof body["MesInfos"].home.map === "function"
+    ) {
+      entries.homes.push({ home: body["MesInfos"].home.map(cleanHomeData) });
     } else {
-      log('info', 'No Home data')
+      log("info", "No Home data");
     }
 
     // Ajout data Foyer
-    entries.foyers = []
-    entries.foyers.push({'foyer': body['MesInfos'].foyer})
+    entries.foyers = [];
+    entries.foyers.push({ foyer: body["MesInfos"].foyer });
 
     // Ajout data ModalitesPaiement
-    entries.paymenttermss = []
-    entries.paymenttermss.push({'paymentterms': body['MesInfos'].paymentTerms})
+    entries.paymenttermss = [];
+    entries.paymenttermss.push({ paymentterms: body["MesInfos"].paymentTerms });
 
     // Ajout data Sinistre
-    let sinistres = body['MesInfos'].insuranceClaim
-    sinistres = sortByDate(sinistres)
-    entries.sinistres = []
-    entries.sinistres.push({'sinistre': sinistres})
+    let sinistres = body["MesInfos"].insuranceClaim;
+    sinistres = sortByDate(sinistres);
+    entries.sinistres = [];
+    entries.sinistres.push({ sinistre: sinistres });
 
     // Ajout data Societaire
-    entries.societaires = []
-    entries.societaires.push({'societaire': body['MesInfos'].client})
+    entries.societaires = [];
+    entries.societaires.push({ societaire: body["MesInfos"].client });
   } else {
-    log('warn', 'No data in the body returned by the MAIF api')
-    return this.terminate('UNKNOWN_ERROR')
+    log("warn", "No data in the body returned by the MAIF api");
+    return this.terminate("UNKNOWN_ERROR");
   }
 
-  return entries
+  return entries;
 }
 
-function sortByDate (data) {
-  if (!data) return []
+function sortByDate(data) {
+  if (!data) return [];
 
-  data.sort(function (a, b) {
-    a = new Date(a.horodatage).getTime()
-    b = new Date(b.horodatage).getTime()
-    return a > b ? -1 : a < b ? 1 : 0
-  })
-  return data
+  data.sort(function(a, b) {
+    a = new Date(a.horodatage).getTime();
+    b = new Date(b.horodatage).getTime();
+    return a > b ? -1 : a < b ? 1 : 0;
+  });
+  return data;
 }
